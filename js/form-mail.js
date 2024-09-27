2466e86e345d11eafc57abd952be5c310612bd26
universe119 <ruddh119@gmail.com>
2024-09-27 13:21:49 +0900

    메일 전송 성공

--- js/form-mail.js
+++ js/form-mail.js
@@ -1,17 +1,24 @@
 emailjs.init({
 	publicKey: "mtp6vqPR3Upyy9wsZ"
 });
+//form선택자 변수 등록
+const form = document.querySelector("#contact-form");
+const user_name = form.querySelector("[name=user_name]");
+const user_email = form.querySelector("[name=user_email]");
+const message = form.querySelector("textarea");
 
-document.getElementById("contact-form").addEventListener("submit", function (event) {
+form.addEventListener("submit", event => {
 	event.preventDefault();
-	// these IDs from the previous steps
-	emailjs.sendForm("service_c2odexb", "template_i3pd7id", this).then(
+	//sendForm(serviceID,  templateID, formEl)
+	emailjs.sendForm("service_c2odexb", "template_i3pd7id", form).then(
 		() => {
-			console.log("SUCCESS!");
+			user_name.value = "";
+			user_email.value = "";
+			message.value = "";
+			alert("문의 내용이 성공적으로 전달되었습니다.");
 		},
 		error => {
-			console.log("FAILED...", error);
+			alert("문의내용 전송에 실패했습니다.");
 		}
 	);
 });
-//3교_btnReset추가_formMail시작_설정_전송까지
